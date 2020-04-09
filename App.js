6a2847d9530530ed957073c0fd90138055b2fcce
vongocphu04 <vongocphu04@gmail.com>
2020-04-09 18:17:21 +0700

    [push duoc notification local]

---
+++ App.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import PushNotification from "react-native-push-notification";
+const App = () => {
+  PushNotification.configure({
+    // (required) Called when a remote or local notification is opened or received
+    onNotification: function(notification) {
+      console.log('LOCAL NOTIFICATION ==>', notification)
+    },
+  popInitialNotification: true,
+    requestPermissions: true
+  })
+  const pushNotificationc = ()=>{
+    
+    PushNotification.localNotification({
+    autoCancel: true,
+    bigText:
+      'This is local notification demo in React Native app. Only shown, when expanded.',
+    subText: 'Local Notification Demo',
+    title: 'Local Notification Title',
+    message: 'Expand me to see more',
+    vibrate: true,
+    vibration: 300,
+    playSound: true,
+    soundName: 'default',
+    actions: '["Yes", "No"]'
+  })
+  }
+  return (
+    <View>
+      <Text>Hello</Text>
+      <Button title="Push" onPress={()=>pushNotificationc()} />
+    </View>
+    
+
+  );
+};
+
+
+export default App;
