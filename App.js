afa12c1f6ff8bb977c9ab68de83dbb16cd099890
vongocphu04 <vongocphu04@gmail.com>
2020-04-09 22:04:09 +0700

    style demo push thong bao

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -15,10 +15,14 @@ import {
   Text,
   StatusBar,
   Button,
+  TextInput,
 } from 'react-native';
 
 import PushNotification from "react-native-push-notification";
 const App = () => {
+  const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
+
   PushNotification.configure({
     // (required) Called when a remote or local notification is opened or received
     onNotification: function(notification) {
@@ -34,8 +38,8 @@ const App = () => {
     bigText:
       'This is local notification demo in React Native app. Only shown, when expanded.',
     subText: 'Local Notification Demo',
-    title: 'Local Notification Title',
-    message: 'Expand me to see more',
+    title: title,
+    message: message,
     vibrate: true,
     vibration: 300,
     playSound: true,
@@ -44,14 +48,33 @@ const App = () => {
   })
   }
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>DEMO PUSH THÔNG BÁO</Text>
+      <Text>Tiêu đề</Text>
+      <TextInput style={styles.input} value={title} onChangeText={(newTitle)=>setTitle(newTitle)}/>
+
+      <Text>Nội dung</Text>
+      <TextInput style={styles.input} value={message} onChangeText={(newMessage)=>setMessage(newMessage)}/>
       <Button title="Push" onPress={()=>pushNotificationc()} />
     </View>
-    
 
   );
 };
-
+const styles = StyleSheet.create({
+  container:{
+    padding:20
+  },
+  input:{
+    height: 40,
+    borderWidth:1,
+    borderColor:'black',
+    margin:5,
+    marginBottom:20
+  },
+  title:{
+    fontSize:18,
+    color:'red'
+  }
+})
 
 export default App;
